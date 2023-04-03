de6aebd1966762af8d8570343083ade7f489c6a7
MoussaGeek <moussadiakite1963@gmail.com>
2023-04-03 09:40:58 +0000

    ajax-task

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :set_blog, only: [:create, :destroy]
 
   def create
     current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
   end
-
+  
   def destroy
     current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
   end
 
+end
+
+private
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
 end
