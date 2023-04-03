7beecf873a0ab454e7d9dcb2cf0c4a54497c39fe
MoussaGeek <moussadiakite1963@gmail.com>
2023-04-03 10:01:28 +0000

    ajax-taskmij

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,12 @@ class FavoritesController < ApplicationController
   def create
     current_user.favorites.create(blog_id: params[:blog_id])
   end
-  
+
   def destroy
     current_user.favorites.find_by(id: params[:id]).destroy
   end
 
-end
-
-private
+  private
 
   def set_blog
     @blog = Blog.find(params[:blog_id])
